c5757fffe7b44891039f12e711cf3271ceea359a
Michelle Borges <102254707+Mikallina@users.noreply.github.com>
2023-12-04 17:47:55 -0300

    Delete UC05 -Projeto Integrador Assist de Adm DB directory

--- UC05 -Projeto Integrador Assist de Adm DB/Atividade 3/item1.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM projetofinal.funcionario;
-
-CREATE USER '11122233344'@'localhost' IDENTIFIED BY 'senha123';
-GRANT SELECT, INSERT, UPDATE, DELETE ON banco_de_dados.* TO '11122233344'@'localhost';
-
--- UC05 -Projeto Integrador Assist de Adm DB/Atividade 3/item4.sql
+++
@@ -1,2 +0,0 @@
-USE projetofinal;
-CREATE INDEX idx_reserva_quarto_data_checkin ON reserva (idquarto, data_check_in);
