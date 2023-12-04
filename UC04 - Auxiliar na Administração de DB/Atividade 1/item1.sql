4dcca7924d63c0d9295193bd32ee9971816de7de
Michelle Borges <102254707+Mikallina@users.noreply.github.com>
2023-12-04 17:46:05 -0300

    Delete UC04 - Auxiliar na Administração de DB directory

--- UC04 - Auxiliar na Administração de DB/Atividade 1/item1.sql
+++
@@ -1,8 +0,0 @@
-CREATE USER 'user_relatorio'@'localhost' IDENTIFIED BY 'senha';
-GRANT SELECT ON uc4atividades.* TO 'user_relatorio'@'localhost';
-
-CREATE ROLE 'role_user_relatorio';
-
-GRANT SELECT ON uc4atividades.* TO 'role_user_relatorio';
-
-GRANT 'role_user_relatorio' TO 'user_relatorio'@'localhost';
